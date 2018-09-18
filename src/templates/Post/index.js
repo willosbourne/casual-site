46730968d9c57994603cb25e02b99d63462bfe62
Will Osbourne <willosbourne@sevenhillstechnology.com>
2018-09-18 08:59:05 -0400

    Contact form on each post (still needs css)

--- src/templates/Post/index.js
+++ src/templates/Post/index.js
@@ -50,6 +50,7 @@ const Post = ({ data, options }) => {
         />
         {isMore ? Button({ path, label: 'MORE', primary: true }) : ''}
         {getAd(isIndex, adsense)}
+        {contactForm()}
       </div>
     </div>
   )
@@ -95,3 +96,26 @@ const Badges = ({ items, primary }) =>
       </span>
     )
   })
+
+const contactForm = () => (
+  <form name="contact" method="POST" netlify>
+    <p>
+      <label>
+        Your Name: <input type="text" name="name" />
+      </label>
+    </p>
+    <p>
+      <label>
+        Your Email: <input type="email" name="email" />
+      </label>
+    </p>
+    <p>
+      <label>
+        Message: <textarea name="message" />
+      </label>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
+)
